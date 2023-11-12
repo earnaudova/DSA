5386186fcdbe1ef764f9336e56fc88793bff875f
Elena <elenanarnaudova@gmail.com>
2023-11-12 23:49:34 +0200

    initial bestSum commit

---
+++ DynamicProgramming/Memoization/bestSum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+/*
+ * bestSum initial structure
+ */
+
+using namespace std;
+
+class Solution{
+public:
+    vector<int> bestSum(int target, vector<int> nums){
+
+        return {};
+    }
+};
