2466f58097f90bcc31e52f6eed1a0f9534783a05
Elena <elenanarnaudova@gmail.com>
2023-11-24 20:59:29 +0100

    implemented tabulation solution for canPartition

--- main.cpp
+++ main.cpp
@@ -9,18 +9,18 @@ int main() {
     int m = 8;
     int n = 8;
     vector<int> nums = {1, 5, 11, 5};
-    vector<int> res;
+    bool res;
     string str = "purple";
     vector<string> wordBank = {"purp", "p", "ur", "le", "purpl"};
 
     Solution* s = new Solution();
 
     res = s->canPartition(nums);
-    for(int i = 0; i < res.size(); i++){
-        cout << res[i] << endl;
-    }
+//    for(int i = 0; i < res.size(); i++){
+//        cout << res[i] << endl;
+//    }
 
-//    cout << res << endl;
+    cout << res << endl;
 
     return 0;
 }
