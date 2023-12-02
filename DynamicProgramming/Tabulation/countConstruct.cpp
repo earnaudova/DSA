3a079009c2c2a1516f3b7f05dcc327d86dc03cfd
Elena <elenanarnaudova@gmail.com>
2023-12-02 21:24:05 +0100

    adding countConstruct to tabulation folder

---
+++ DynamicProgramming/Tabulation/countConstruct.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <string>
+
+/*
+    Function should return number of ways that the 'target'
+    can be constructed by concatenating elements of the
+    'wordBank' array.
+
+    Elements in wordBank may be reused.
+
+    m = target.length()
+    n = wordBank.length()
+
+    O() time
+    O() space
+ */
+
+using namespace std;
+
+class Solution{
+public:
+    int countConstruct(string target, vector<string> &wordBank){
+        return 0;
+    }
+};
